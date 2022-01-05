e2e89e936f72afb9315a1186fce33d4a873c220e
batphonghan <vanhungdeveloper@gmail.com>
2022-01-05 22:44:57 +0700

    CLI submit scavenge

--- x/scavenge/client/cli/tx_submit_scavenge.go
+++ x/scavenge/client/cli/tx_submit_scavenge.go
@@ -1,6 +1,8 @@
 package cli
 
 import (
+	"crypto/sha256"
+	"encoding/hex"
 	"strconv"
 
 	"github.com/batphonghan/blog/x/scavenge/types"
@@ -18,24 +20,28 @@ func CmdSubmitScavenge() *cobra.Command {
 		Short: "Broadcast message submit-scavenge",
 		Args:  cobra.ExactArgs(3),
 		RunE: func(cmd *cobra.Command, args []string) (err error) {
-			argSolutionHash := args[0]
-			argDescription := args[1]
-			argReward := args[2]
-
 			clientCtx, err := client.GetClientTxContext(cmd)
 			if err != nil {
 				return err
 			}
 
+			// find a hash of the solution
+			solutionHash := sha256.Sum256([]byte(args[0]))
+			// convert the hash to string
+			solutionHashString := hex.EncodeToString(solutionHash[:])
+			argsDescription := string(args[1])
+			argsReward := string(args[2])
+
 			msg := types.NewMsgSubmitScavenge(
 				clientCtx.GetFromAddress().String(),
-				argSolutionHash,
-				argDescription,
-				argReward,
-			)
+				string(solutionHashString),
+				string(argsDescription),
+				string(argsReward))
+
 			if err := msg.ValidateBasic(); err != nil {
 				return err
 			}
+
 			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msg)
 		},
 	}
